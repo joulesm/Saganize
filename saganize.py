de9dfe94aea8274f645d15277e9f99a3d6e27d97
joulesm <joulesm@gmail.com>
2014-10-04 15:25:44 -0700

    adds flask debugger,

--- saganize.py
+++ saganize.py
@@ -2,8 +2,16 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def homepage():
+    return 'Saganize this!!'
+
+@app.route('/search')
+def search():
+    return 'saganized-search'
+
 
 if __name__ == '__main__':
-    app.run()
+    # auto reloads the server on changes, also enables debugging
+    app.debug = True
+    # server is publicly available
+    app.run(host='0.0.0.0')
