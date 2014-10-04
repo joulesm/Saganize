f6189047b1d3bfc44f52cf93c9781d8fb70990d1
joulesm <joulesm@gmail.com>
2014-10-04 16:31:43 -0700

    add google search; returns vanilla search page

--- saganize.py
+++ saganize.py
@@ -1,8 +1,18 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+import httplib
 
 app = Flask(__name__)
+
+def google_search(query):
+    url = "/search?q=" + query
+    conn = httplib.HTTPConnection("www.google.com")
+    conn.request("GET", url)
+    res = conn.getresponse()
+    data = res.read()
+    conn.close()
+    return data
 
 @app.route('/')
 def homepage():
@@ -11,8 +21,8 @@ def homepage():
 @app.route('/search')
 @app.route('/search/<query>')
 def do_search(query=None):
-    #return 'saganized-search on %s' % query
-    return render_template('search.html', query = query)
+    return google_search(query)
+    #return render_template('search.html', query = query)
 
 if __name__ == '__main__':
     # auto reloads the server on changes, also enables debugging
