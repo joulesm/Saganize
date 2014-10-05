ff9ede1aa5a086b8e21a84737e2ef5a8f35a813f
joulesm <joulesm@gmail.com>
2014-10-04 20:03:57 -0700

    randomly add science term to search query

--- saganize.py
+++ saganize.py
@@ -1,8 +1,12 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+
 import httplib
 import urllib
+import random
+
+import science_list
 
 app = Flask(__name__)
 
@@ -16,7 +20,7 @@ def google_search(query):
     return fix_images(saganize_searchpage(data))
 
 def saganize_searchpage(data):
-    return replace_num_results(fix_images(data))
+    return replace_num_results(replace_shopping(fix_images(data)))
 
 def replace_num_results(data):
     sI = data.find("class=\"sd\"") + 28
@@ -24,11 +28,18 @@ def replace_num_results(data):
     replace_results = data[sI : eI]
     return data.replace(replace_results, "Billions and billions of results")
 
+def replace_shopping(data):
+    return data.replace("Shopping", "<b>SCIENCE!!</b>")
+
 def fix_images(data):
     img = '<a href="/webhp?hl=en" style="background:url(/images/nav_logo176.png) no-repeat 0 -41px;height:37px;width:95px;display:block" id="logo" title="Go to Google Home"></a>'
-    img_replace = '<a href="www.google.com/search?q=carl+sagan"><img src="http://carlsaganday.com/wp-content/uploads/2013/09/carlsagan.jpg" height="37px" width="95px"></a>'
+    img_replace = '<a href="www.google.com/search?q=carl+sagan" style="margin-left:12px;margin-top:12px;position:absolute;"><img src="http://carlsaganday.com/wp-content/uploads/2013/09/carlsagan.jpg" height="37px" width="95px"></a>'
     return data.replace(img, img_replace)
-    #return data.replace("/images/nav_logo176.png", "http://www.google.com/images/nav_logo176.png")
+    #return data.replace("/images/nav_logo176.png", "http://www.google.com/images/nav_logo176.png").replace("http://www.google.comhttp://www.google.com", "http://www.google.com")
+    #return data
+
+def sciencify_query(query):
+    return query + ' ' + science_list.sList[random.randint(0, len(science_list.sList) - 1)]
 
 @app.route('/')
 def homepage():
@@ -37,8 +48,7 @@ def homepage():
 @app.route('/search')
 @app.route('/search/<query>')
 def do_search(query=None):
-    science_query = query + ' carl sagan'
-    return google_search(science_query)
+    return google_search(sciencify_query(query))
     #return render_template('search.html', query = query)
 
 if __name__ == '__main__':
